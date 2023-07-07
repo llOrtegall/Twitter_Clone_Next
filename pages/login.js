a2e087909e00c1c345f848cd2aac34a283328c5c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-07 11:09:25 -0500

    agregando provider login

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,17 @@
+import { getProviders } from "next-auth/react";
+
 export default function LoginPage() {
   return (
-    <div>login page here</div>
-  );
+    <div className="flex items-center justify-center h-screen">
+
+    </div>
+  )
+
+} // close home
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  }
 }
