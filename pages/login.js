c9ea4e3708115cf6ae5c6c725649467548baa18c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-07 11:25:36 -0500

    creando el loing con gogole ok

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,15 @@
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
 export default function LoginPage({ providers }) {
   return (
     <div className="flex items-center justify-center h-screen">
       {Object.values(providers).map(provider => (
         <div>
-          <button className="">Sig in with {provider.name}</button>
+          <button onClick={async () => { await signIn(provider.id) }}
+            className="bg-twitterWhite pl-3 pr-5 py-2 text-black rounded-full flex items-center">
+            <img src="google.png" alt="" className="h-8" />
+            Sig in with {provider.name}
+          </button>
         </div>
       ))}
     </div>
